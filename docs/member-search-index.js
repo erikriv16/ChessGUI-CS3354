85cf2aa0a77060bc772d3f04563268b04296c754
Larry Lopez <lopez.larry@outlook.com>
2024-11-02 00:13:19 -0500

    v1.0.0

---
+++ docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"ChessBoardWithPiecesExample","l":"ChessBoardWithPiecesExample()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"ChessBoardWithPiecesExample","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
